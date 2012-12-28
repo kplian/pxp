e2e76607e6d4ba7ff2a7a8febf838a88effad8a5
Rodrigo Chumacero <rodrigo@kplian.com>
2012-12-28 16:19:37 -0400

    -#93 Generar script de datos de Mantenimiento Asociado al issue de mantenimiento

--- sis_parametros/base/datos.sql
+++ sis_parametros/base/datos.sql
@@ -327,20 +327,16 @@ INSERT INTO param.tmoneda ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fec
 VALUES (1, NULL, E'2012-11-08 00:00:00', E'2012-11-08 12:07:11.620', E'activo', 2, E'Dólares Americanos', E'USD', NULL);
 
 
-/* UNIDADES DE MEDIDA*/ 
+/* UNIDADES DE MEDIDA*/
 INSERT INTO param.tunidad_medida ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_unidad_medida", "codigo", "descripcion", "tipo")
-VALUES (1, NULL, E'2012-11-02 13:31:27', E'2012-11-02 13:31:27', E'activo', 9, E'Km.', E'Kilometro','Longitud');
+VALUES (1, 1, E'2012-11-02 13:26:52', E'2012-12-19 18:00:08.498', E'activo', 1, E'Hr.', 'Hora', E'Tiempo');
 INSERT INTO param.tunidad_medida ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_unidad_medida", "codigo", "descripcion", "tipo")
-VALUES (1, 1, E'2012-11-02 13:29:13', E'2012-11-03 09:42:35.594', E'activo', 2, E'dia', E'Dia','Tiempo');
+VALUES (1, 1, E'2012-11-02 13:29:13', E'2012-12-19 18:00:55.574', E'activo', 2, E'dia', 'Día', E'Tiempo');
 INSERT INTO param.tunidad_medida ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_unidad_medida", "codigo", "descripcion", "tipo")
-VALUES (1, 1, E'2012-11-02 13:26:52', E'2012-11-03 09:42:43.275', E'activo', 1, E'Hr.', E'Hora','Tiempo');
+VALUES (1, 1, E'2012-11-02 13:34:20', E'2012-12-19 18:01:24.560', E'activo', 3, E'sem', 'Semana', E'Tiempo');
 INSERT INTO param.tunidad_medida ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_unidad_medida", "codigo", "descripcion", "tipo")
-VALUES (1, 1, E'2012-11-02 13:30:42', E'2012-11-03 09:42:54.058', E'activo', 5, E'Año', E'Año','Tiempo');
+VALUES (1, 1, E'2012-11-02 13:29:13', E'2012-12-19 18:50:36.757', E'activo', 4, E'mes', 'Mes', E'Tiempo');
 INSERT INTO param.tunidad_medida ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_unidad_medida", "codigo", "descripcion", "tipo")
-VALUES (1, 1, E'2012-11-02 13:29:13', E'2012-11-03 09:43:44.843', E'activo', 4, E'mes', E'Mes','Tiempo');
+VALUES (1, 1, E'2012-11-02 13:30:42', E'2012-12-19 19:05:56.585', E'activo', 5, E'año', 'Año', E'Tiempo');
 INSERT INTO param.tunidad_medida ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_unidad_medida", "codigo", "descripcion", "tipo")
-VALUES (1, 1, E'2012-11-02 13:34:20', E'2012-11-03 09:46:29.882', E'activo', 3, E'sem', E'Semana','Tiempo');
-
-
-
-
+VALUES (1, NULL, E'2012-11-02 13:31:27', E'2012-11-02 13:31:27', E'activo', 9, E'Km', 'Kilómetros', E'Longitud');
