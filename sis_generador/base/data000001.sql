2fc6873d55c179c3b78597974d8a8eba1caed1bf
Rodrigo Chumacero <rodrigo@kplian.com>
2013-01-14 15:14:20 -0400

    Organización de archivos en data, dependencies, patch

---
+++ sis_generador/base/data000001.sql
@@ -0,0 +1,18 @@
+/***********************************I-SCP-RCM-GEN-0-14/01/2013****************************************/
+INSERT INTO segu.tsubsistema ( codigo, nombre, fecha_reg, prefijo, estado_reg, nombre_carpeta, id_subsis_orig)
+VALUES ('GEN', 'Sistema Generador de Codigo', '2009-11-02', 'GEN', 'activo', 'generador', NULL);
+
+select pxp.f_insert_tfuncion ('ft_tabla_sel', 'Funcion para tabla', 'GEN');
+select pxp.f_insert_tfuncion ('ft_tabla_ime', 'Funcion para tabla', 'GEN');
+select pxp.f_insert_tfuncion ('ft_columna_ime', 'Funcion para tabla', 'GEN');
+select pxp.f_insert_tfuncion ('ft_columna_sel', 'Funcion para tabla', 'GEN');
+select pxp.f_insert_tfuncion ('ft_esquema_sel', 'Funcion para tabla', 'GEN');
+select pxp.f_insert_tgui ('Columnas', 'Registro de las Columnas de las tablas', 'COL', 'no', 1, 'sis_generador/vista/columna/columna.js', 4, '', 'columna', 'GEN');
+select pxp.f_insert_tgui ('Procesos', '', 'PROCGEN', 'si', 2, '', 2, '', '', 'GEN');
+select pxp.f_insert_tgui ('GEN', 'Generador de Código', 'GEN', 'si', 3, '', 1, '../../../lib/imagenes/gen32x32.png', '', 'GEN');
+select pxp.f_insert_tgui ('Generador', 'Registro de las tablas', 'TABLA', 'si', 1, 'sis_generador/vista/tabla/tabla.js', 3, '', 'tabla', 'GEN');
+select pxp.f_insert_testructura_gui ('GEN', 'SISTEMA');
+select pxp.f_insert_testructura_gui ('PROCGEN', 'GEN');
+select pxp.f_insert_testructura_gui ('TABLA', 'PROCGEN');
+select pxp.f_insert_testructura_gui ('COL', 'TABLA');
+/***********************************F-SCP-RCM-GEN-0-14/01/2013****************************************/
---
+++ sis_generador/base/dependencies000001.sql
@@ -0,0 +1,33 @@
+/***********************************I-DEP-RCM-GEN-0-14/01/2013****************************************/
+ALTER TABLE ONLY gen.tcolumna
+    ADD CONSTRAINT fk_tcolumna__id_tabla
+    FOREIGN KEY (id_tabla) REFERENCES gen.ttabla(id_tabla);
+    
+ALTER TABLE ONLY gen.ttabla
+    ADD CONSTRAINT fk_ttabla__id_subsistema
+    FOREIGN KEY (id_subsistema) REFERENCES segu.tsubsistema(id_subsistema);
+
+CREATE VIEW gen.vcolumna AS
+SELECT col.table_schema AS esquema, col.table_name AS tabla,
+    col.column_name AS columna, (col.ordinal_position)::integer AS
+    posicion, (col.column_default)::character varying AS defecto,
+    (col.is_nullable)::character varying AS blanco,
+    (col.data_type)::character varying AS tipo, CASE WHEN
+    ((col.data_type)::text = 'character varying'::text) THEN
+    (col.character_maximum_length)::integer WHEN ((col.data_type)::text =
+    'numeric'::text) THEN (col.numeric_precision)::integer ELSE 0 END AS
+    length, CASE WHEN ((col.data_type)::text = 'numeric'::text) THEN
+    (col.numeric_scale)::integer ELSE 0 END AS "precision",
+    (cons.conname)::character varying AS nombre_constraint,
+    (cons.consrc)::character varying AS definicion_constraint
+FROM (((information_schema.columns col LEFT JOIN
+    information_schema.constraint_column_usage colcon ON
+    (((((col.table_schema)::text = (colcon.table_schema)::text) AND
+    ((col.table_name)::text = (colcon.table_name)::text)) AND
+    ((col.column_name)::text = (colcon.column_name)::text)))) LEFT JOIN
+    pg_constraint cons ON (((cons.conname = (colcon.constraint_name)::name)
+    AND (cons.contype = 'c'::"char")))) LEFT JOIN pg_class c ON
+    (((cons.conrelid = c.oid) AND (c.relname = (col.table_name)::name))))
+WHERE (((col.table_schema)::text <> 'pg_catalog'::text) AND
+    ((col.table_schema)::text <> 'information_schema'::text));
+/***********************************I-DEP-RCM-GEN-0-14/01/2013****************************************/
