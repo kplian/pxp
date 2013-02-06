33688d5d1c706830250a2c83066c72e0ebc89076
gonzaloSarmientoSejas <gonzalo@kplian.com>
2013-02-06 16:58:11 -0400

    -#97: Registro de funcionarios: independizar y aumentar campos para horarios estaticos: Modificacion para permitir aumentar campos en las clases heredadas.

--- sis_organigrama/modelo/MODFuncionarioEspecialidad.php
+++ sis_organigrama/modelo/MODFuncionarioEspecialidad.php
@@ -34,6 +34,7 @@ class MODFuncionarioEspecialidad extends MODbase{
 		
 		//Ejecuta la instruccion
 		$this->armarConsulta();
+		//echo $this->consulta;exit;
 		$this->ejecutarConsulta();
 		
 		//Devuelve la respuesta
