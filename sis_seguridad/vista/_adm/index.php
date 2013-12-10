4c706ee15dd524d60402ef2133b143b38a73a835
Rensi Arteaga Copar <rensi@kplian.com>
2013-12-10 10:49:24 -0400

    rebision grupo ep

--- sis_seguridad/vista/_adm/index.php
+++ sis_seguridad/vista/_adm/index.php
@@ -73,10 +73,10 @@ if(!isset($_SESSION["_SESION"])){
     <script type="text/javascript" charset="UTF-8" src="../../../lib/ext3/builds/ext-core.js"></script>
     -->
     <script type="text/javascript" charset="UTF-8" src="../../../lib/ext3/ext-all-debug.js"></script>
-    
+    <!--   
     <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-     <!--   
+     
      	
      	
      	<script type="text/javascript" src="resources/TaskBar.js"></script> -->
