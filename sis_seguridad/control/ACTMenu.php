f39184f47a54e38c8c8b46f16e87e4594fbfd26d
dora <dcastro@boa.bo>
2014-04-15 15:30:04 -0400

    anadiendo tooltip

--- sis_seguridad/control/ACTMenu.php
+++ sis_seguridad/control/ACTMenu.php
@@ -41,6 +41,7 @@ class ACTMenu extends ACTbase {
 		$arreglo=array();
 		array_push($arreglo,array('nombre'=>'id','valor'=>'id_gui'));
 		array_push($arreglo,array('nombre'=>'text','valor'=>'nombre'));
+		array_push($arreglo,array('nombre'=>'qtip','valor'=>'descripcion'));
 		array_push($arreglo,array('nombre'=>'ruta','valor'=>'ruta_archivo'));
 		
 		
