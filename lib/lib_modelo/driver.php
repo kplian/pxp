9140bd940b8c42e6dee78bf684d3b4eab4205529
dora <dcastro@boa.bo>
2014-06-10 14:35:41 -0400

    revision documento wf se;

--- lib/lib_modelo/driver.php
+++ lib/lib_modelo/driver.php
@@ -95,7 +95,13 @@ class driver
                 $this->nom_usuario_ai=$_SESSION["_NOM_USUARIO_AI"];
         }
 		
-		$this->ip=getenv("REMOTE_ADDR");
+		if (getenv('HTTP_X_FORWARDED_FOR')){
+		    $this->ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} 
+		else{ 
+		    $this->ip = $_SERVER['REMOTE_ADDR'];
+		}
+		
 		$this->mac="99:99:99:99:99:99";
 		
 		$this->consulta_armada=false;
