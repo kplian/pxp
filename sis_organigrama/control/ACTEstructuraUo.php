6936eadc87a013582a210853735907c1748dbee3
dora <dcastro@boa.bo>
2014-07-02 17:53:54 -0400

    corrigiendo filtro de arbol uo

--- sis_organigrama/control/ACTEstructuraUo.php
+++ sis_organigrama/control/ACTEstructuraUo.php
@@ -43,8 +43,7 @@ class ACTEstructuraUo extends ACTbase {
 			$this -> res = $this -> objFunSeguridad -> listarUoFiltro($this -> objParam);
 			
 			if ($this -> res) {
-				var_dump($this->res);
-				exit;
+				
 				foreach ($this->res->datos as $f) {
 					//var_dump($f);
 					if ($pri == 1) {
