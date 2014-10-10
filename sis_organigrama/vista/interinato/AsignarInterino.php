4a2c2cc89e64e348acc91ab00ac255c2f61ceb1d
dora <dcastro@boa.bo>
2014-10-10 15:48:57 -0400

    Resolviendo problema con listado de interinatos

--- sis_organigrama/vista/interinato/AsignarInterino.php
+++ sis_organigrama/vista/interinato/AsignarInterino.php
@@ -31,7 +31,7 @@ Phx.vista.AsignarInterino = {
 	  this.Atributos[this.getIndAtributo('id_cargo_titular')].form=false;
  
 	  Phx.vista.AsignarInterino.superclass.constructor.call(this,config);      
-        
+      this.load({params:{start:0, limit:this.tam_pag}});  
     },
     
     ActSave:'../../sis_organigrama/control/Interinato/asignarMiSuplente',
