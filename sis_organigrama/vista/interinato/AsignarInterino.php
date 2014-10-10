40a9c9b7f830e79797de84160a96b67723949fc0
dora <dcastro@boa.bo>
2014-10-10 16:03:42 -0400

    arreglando problema con interinato

--- sis_organigrama/vista/interinato/AsignarInterino.php
+++ sis_organigrama/vista/interinato/AsignarInterino.php
@@ -30,7 +30,8 @@ Phx.vista.AsignarInterino = {
 	   
 	  this.Atributos[this.getIndAtributo('id_cargo_titular')].form=false;
  
-	  Phx.vista.AsignarInterino.superclass.constructor.call(this,config);      
+	  Phx.vista.AsignarInterino.superclass.constructor.call(this,config); 
+	  this.store.baseParams = {id_cargo_suplente:Phx.CP.config_ini.id_cargo, estado_reg:'activo'}      
       this.load({params:{start:0, limit:this.tam_pag}});  
     },
     
