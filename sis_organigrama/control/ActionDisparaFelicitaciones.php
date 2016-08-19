ed026a85ca4a5bb059ed2b4ed1bb0fe02b9a5a1f
Jaime Rivera <rensi4rn@gmail.com>
2016-08-19 10:36:18 -0400

    se cambio action dispara alerta cumpleaneros a rest

--- sis_organigrama/control/ActionDisparaFelicitaciones.php
+++ sis_organigrama/control/ActionDisparaFelicitaciones.php
@@ -1,57 +1,43 @@
 <?php 
 /*** 
  Nombre: Intermediario.php
- Proposito: revisa los pagos penxdientes al dia e insertar las alamras correspondientes
+ Proposito: Invocar al disparador de alarmas saltandose los pasos de 
+ *          autentificacion
+ * 		    este archivo se  invoca desde un cron tab en servidor linux
+ *          solo deberia llamarse desde ahí, otras llamadas no seran autorizadas 
  Autor:	Kplian (RAC)
- Fecha:	14/10/2014 */
+ Fecha:	19/07/2010
+ */
+
 include_once(dirname(__FILE__)."/../../lib/lib_control/CTSesion.php");
 session_start();
 $_SESSION["_SESION"]= new CTSesion(); 
+
 include(dirname(__FILE__).'/../../lib/DatosGenerales.php');
 include_once(dirname(__FILE__).'/../../lib/lib_general/Errores.php');
-include_once(dirname(__FILE__).'/../../lib/PHPMailer/class.phpmailer.php');
-include_once(dirname(__FILE__).'/../../lib/PHPMailer/class.smtp.php');
-include_once(dirname(__FILE__).'/../../lib/lib_general/cls_correo_externo.php');
 include_once(dirname(__FILE__).'/../../lib/rest/PxpRestClient.php');
 
 
-
-include_once(dirname(__FILE__).'/../../lib/FirePHPCore-0.3.2/lib/FirePHPCore/FirePHP.class.php');
-
-
 ob_start();
-$fb=FirePHP::getInstance(true);
+
 
 //estable aprametros ce la cookie de sesion
 $_SESSION["_CANTIDAD_ERRORES"]=0;//inicia control 
 
 
+//echo dirname(__FILE__).'LLEGA';
 register_shutdown_function('fatalErrorShutdownHandler');
 set_exception_handler('exception_handler');
 set_error_handler('error_handler');;
 include_once(dirname(__FILE__).'/../../lib/lib_control/CTincludes.php');
-include_once(dirname(__FILE__).'/../../sis_organigrama/modelo/MODFuncionario.php');
 
+$pxpRestClient = PxpRestClient::connect('127.0.0.1',substr($_SESSION["_FOLDER"], 1) .'pxp/lib/rest/')
+                                                                                                        ->setCredentialsPxp($_GET['user'],$_GET['pw']);
+$fecha = new DateTime();
+$res = $pxpRestClient->doPost('organigrama/Funcionario/alertarCumpleaneroDia',
+				    array()); 
+$res_json = json_decode($res);	
 
-        $objPostData=new CTPostData();
-        $arr_unlink=array();
-        $aPostData=$objPostData->getData();
-        $_SESSION["_PETICION"]=serialize($aPostData);
-        $objParam = new CTParametro($aPostData['p'],null,$aPostFiles);
-        ////////////////
-        $objParam->defecto('ordenacion','id_plan_pago');
-        $objParam->defecto('dir_ordenacion','asc');
-        $objFunc=new MODFuncionario($objParam);    
-        
-		//listas plan es de pago pendientes
-		$res=$objFunc->alertarCumpleaneroDia();
-		if($res->getTipo()=='ERROR'){
-			echo 'Se ha producido un error-> Mensaje Técnico:'.$res->getMensajeTec();
-			exit;        
-		} 
-       else{
-       	   echo 'Procesado con exito';
-       }
-	      
- 
+var_dump($res_json);
+exit;	
 ?>
