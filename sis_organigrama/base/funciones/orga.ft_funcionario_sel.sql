1cb0d740b75c279b53c1f538c7bcb816d64fc5ae
Jaime Rivera <rensi4rn@gmail.com>
2016-12-12 11:43:11 -0400

    cambiando listado de funcionarios solo para q saque numeros celulares personales

--- sis_organigrama/base/funciones/orga.ft_funcionario_sel.sql
+++ sis_organigrama/base/funciones/orga.ft_funcionario_sel.sql
@@ -4,53 +4,53 @@ CREATE OR REPLACE FUNCTION orga.ft_funcionario_sel (
   par_tabla varchar,
   par_transaccion varchar
 )
-RETURNS varchar AS
-$body$
-/**************************************************************************
- FUNCION: 		orga.ft_funcionario_sel 
- DESCRIPCIÓN:  listado de funcionario
- AUTOR: 	    KPLIAN (mzm)
- FECHA:	        
- COMENTARIOS:	
-***************************************************************************
- HISTORIA DE MODIFICACIONES:
+  RETURNS varchar AS
+  $body$
+  /**************************************************************************
+   FUNCION: 		orga.ft_funcionario_sel
+   DESCRIPCIÓN:  listado de funcionario
+   AUTOR: 	    KPLIAN (mzm)
+   FECHA:
+   COMENTARIOS:
+  ***************************************************************************
+   HISTORIA DE MODIFICACIONES:
 
- DESCRIPCION:	
- AUTOR:		
- FECHA:		21-01-2011
-***************************************************************************/
-
-
-DECLARE
+   DESCRIPCION:
+   AUTOR:
+   FECHA:		21-01-2011
+  ***************************************************************************/
 
 
-v_consulta         varchar;
-v_parametros       record;
-v_nombre_funcion   text;
-v_mensaje_error    text;
-v_resp             varchar;
-v_filadd           varchar;
-v_id_funcionario	integer;
-v_ids_funcionario	varchar;
+  DECLARE
 
 
-BEGIN
+    v_consulta         varchar;
+    v_parametros       record;
+    v_nombre_funcion   text;
+    v_mensaje_error    text;
+    v_resp             varchar;
+    v_filadd           varchar;
+    v_id_funcionario	integer;
+    v_ids_funcionario	varchar;
 
-     v_parametros:=pxp.f_get_record(par_tabla);
-     v_nombre_funcion:='orga.ft_funcionario_sel';
 
-/*******************************
- #TRANSACCION:  RH_FUNCIO_SEL
- #DESCRIPCION:	Listado de funcionarios
- #AUTOR:		
- #FECHA:		23/05/11	
-***********************************/
-     if(par_transaccion='RH_FUNCIO_SEL')then
+  BEGIN
 
-          --consulta:=';
-          BEGIN
+    v_parametros:=pxp.f_get_record(par_tabla);
+    v_nombre_funcion:='orga.ft_funcionario_sel';
 
-               v_consulta:='SELECT 
+    /*******************************
+     #TRANSACCION:  RH_FUNCIO_SEL
+     #DESCRIPCION:	Listado de funcionarios
+     #AUTOR:
+     #FECHA:		23/05/11
+    ***********************************/
+    if(par_transaccion='RH_FUNCIO_SEL')then
+
+      --consulta:=';
+      BEGIN
+
+        v_consulta:='SELECT
                             FUNCIO.id_funcionario,
                             FUNCIO.codigo,
                             FUNCIO.estado_reg,
@@ -87,41 +87,41 @@ BEGIN
                             inner join segu.tusuario usu1 on usu1.id_usuario = FUNCIO.id_usuario_reg
 						    left join segu.tusuario usu2 on usu2.id_usuario = FUNCIO.id_usuario_mod
                             WHERE ';
-               
-               
-               
-               v_consulta := v_consulta || v_parametros.filtro;
-               
-               if (pxp.f_existe_parametro(par_tabla, 'tipo') and
-				pxp.f_existe_parametro(par_tabla, 'fecha') and 
-				pxp.f_existe_parametro(par_tabla, 'id_uo')) then
-				
-				if (v_parametros.tipo is not null and v_parametros.tipo = 'oficial' and v_parametros.fecha is not null and v_parametros.id_uo is not null) then
-					v_ids_funcionario = orga.f_get_funcionarios_con_asignacion_activa(v_parametros.id_uo, v_parametros.fecha);
-					v_consulta := v_consulta || ' and FUNCIO.id_funcionario not in (' || v_ids_funcionario ||') ';
-					
-				end if;
-			end if;
-               
-               v_consulta:=v_consulta||' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' OFFSET ' || v_parametros.puntero;
-
-               return v_consulta;
 
 
-         END;
 
-/*******************************
- #TRANSACCION:  RH_FUNCIO_CONT
- #DESCRIPCION:	Conteo de funcionarios
- #AUTOR:		
- #FECHA:		23/05/11	
-***********************************/
-     elsif(par_transaccion='RH_FUNCIO_CONT')then
+        v_consulta := v_consulta || v_parametros.filtro;
 
-          --consulta:=';
-          BEGIN
+        if (pxp.f_existe_parametro(par_tabla, 'tipo') and
+            pxp.f_existe_parametro(par_tabla, 'fecha') and
+            pxp.f_existe_parametro(par_tabla, 'id_uo')) then
 
-               v_consulta:='SELECT
+          if (v_parametros.tipo is not null and v_parametros.tipo = 'oficial' and v_parametros.fecha is not null and v_parametros.id_uo is not null) then
+            v_ids_funcionario = orga.f_get_funcionarios_con_asignacion_activa(v_parametros.id_uo, v_parametros.fecha);
+            v_consulta := v_consulta || ' and FUNCIO.id_funcionario not in (' || v_ids_funcionario ||') ';
+
+          end if;
+        end if;
+
+        v_consulta:=v_consulta||' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' OFFSET ' || v_parametros.puntero;
+
+        return v_consulta;
+
+
+      END;
+
+    /*******************************
+     #TRANSACCION:  RH_FUNCIO_CONT
+     #DESCRIPCION:	Conteo de funcionarios
+     #AUTOR:
+     #FECHA:		23/05/11
+    ***********************************/
+    elsif(par_transaccion='RH_FUNCIO_CONT')then
+
+      --consulta:=';
+      BEGIN
+
+        v_consulta:='SELECT
                                   count(FUNCIO.id_funcionario)
                             FROM orga.tfuncionario FUNCIO
                             INNER JOIN SEGU.vpersona PERSON ON PERSON.id_persona=FUNCIO.id_persona
@@ -130,31 +130,31 @@ BEGIN
                             inner join segu.tusuario usu1 on usu1.id_usuario = FUNCIO.id_usuario_reg
 						    left join segu.tusuario usu2 on usu2.id_usuario = FUNCIO.id_usuario_mod
                             WHERE ';
-               v_consulta:=v_consulta||v_parametros.filtro;
-               if (pxp.f_existe_parametro(par_tabla, 'tipo') and
-				pxp.f_existe_parametro(par_tabla, 'fecha') and 
-				pxp.f_existe_parametro(par_tabla, 'id_uo')) then
-				
-				if (v_parametros.tipo is not null and v_parametros.tipo = 'oficial' and v_parametros.fecha is not null and v_parametros.id_uo is not null) then
-					v_ids_funcionario = orga.f_get_funcionarios_con_asignacion_activa(v_parametros.id_uo, v_parametros.fecha);
-					v_consulta := v_consulta || ' and FUNCIO.id_funcionario not in (' || v_ids_funcionario ||') ';
-					
-				end if;
-			end if;
-               return v_consulta;
-         END;
- /*******************************
- #TRANSACCION:  RH_GETDAFUN_SEL
- #DESCRIPCION:	Obtener datos de funcionario a partir del nombre
- #AUTOR:		
- #FECHA:		23/05/11	
-***********************************/
-     elsif(par_transaccion='RH_GETDAFUN_SEL')then
+        v_consulta:=v_consulta||v_parametros.filtro;
+        if (pxp.f_existe_parametro(par_tabla, 'tipo') and
+            pxp.f_existe_parametro(par_tabla, 'fecha') and
+            pxp.f_existe_parametro(par_tabla, 'id_uo')) then
 
-          --consulta:=';
-          BEGIN
+          if (v_parametros.tipo is not null and v_parametros.tipo = 'oficial' and v_parametros.fecha is not null and v_parametros.id_uo is not null) then
+            v_ids_funcionario = orga.f_get_funcionarios_con_asignacion_activa(v_parametros.id_uo, v_parametros.fecha);
+            v_consulta := v_consulta || ' and FUNCIO.id_funcionario not in (' || v_ids_funcionario ||') ';
 
-               v_consulta:='SELECT 
+          end if;
+        end if;
+        return v_consulta;
+      END;
+    /*******************************
+    #TRANSACCION:  RH_GETDAFUN_SEL
+    #DESCRIPCION:	Obtener datos de funcionario a partir del nombre
+    #AUTOR:
+    #FECHA:		23/05/11
+   ***********************************/
+    elsif(par_transaccion='RH_GETDAFUN_SEL')then
+
+      --consulta:=';
+      BEGIN
+
+        v_consulta:='SELECT
                				FUNCIO.id_funcionario,
                             PERSON.nombre_completo1::varchar,
                             CAR.nombre,
@@ -170,39 +170,39 @@ BEGIN
                             INNER JOIN orga.tcargo car on car.id_cargo = uofun.id_cargo
                             LEFT JOIN gecom.tfuncionario_celular fc on fc.id_funcionario = FUNCIO.id_funcionario
                             	and fc.estado_reg = ''activo'' and fc.fecha_inicio <= now()::date and
-                                (fc.fecha_fin >= now()::date or fc.fecha_fin is null)
+                                (fc.fecha_fin >= now()::date or fc.fecha_fin is null) and fc.tipo_asignacion = ''personal''
                             LEFT JOIN gecom.tnumero_celular nc ON
                             	nc.id_numero_celular = fc.id_numero_celular and nc.tipo = ''celular''
                             WHERE ';
-               
-               
-               
-               v_consulta := v_consulta || v_parametros.filtro;
-               v_consulta := v_consulta ||  ' GROUP BY FUNCIO.id_funcionario,
+
+
+
+        v_consulta := v_consulta || v_parametros.filtro;
+        v_consulta := v_consulta ||  ' GROUP BY FUNCIO.id_funcionario,
                             PERSON.nombre_completo1,
                             CAR.nombre,                            
                             FUNCIO.email_empresa';
-              
-               
-               v_consulta:=v_consulta||' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' OFFSET ' || v_parametros.puntero;
-
-               return v_consulta;
 
 
-         END;
+        v_consulta:=v_consulta||' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' OFFSET ' || v_parametros.puntero;
 
-/*******************************
- #TRANSACCION:  RH_GETDAFUN_CONT
- #DESCRIPCION:	Conteo de registros al obtener datos de funcionario a partir del nombre
- #AUTOR:		
- #FECHA:		23/05/11	
-***********************************/
-     elsif(par_transaccion='RH_GETDAFUN_CONT')then
+        return v_consulta;
 
-          --consulta:=';
-          BEGIN
 
-               v_consulta:='SELECT 
+      END;
+
+    /*******************************
+     #TRANSACCION:  RH_GETDAFUN_CONT
+     #DESCRIPCION:	Conteo de registros al obtener datos de funcionario a partir del nombre
+     #AUTOR:
+     #FECHA:		23/05/11
+    ***********************************/
+    elsif(par_transaccion='RH_GETDAFUN_CONT')then
+
+      --consulta:=';
+      BEGIN
+
+        v_consulta:='SELECT
                				count(FUNCIO.id_funcionario)
                                                         
                             FROM orga.tfuncionario FUNCIO
@@ -214,28 +214,28 @@ BEGIN
                             INNER JOIN orga.tcargo car on car.id_cargo = uofun.id_cargo
                             
                             WHERE ';
-               
-               
-               
-               v_consulta := v_consulta || v_parametros.filtro;
-               
-               return v_consulta;
 
 
-         END;
 
-/*******************************
- #TRANSACCION:  RH_GETCUMPLEA_SEL
- #DESCRIPCION:	Cumpleaneros a fecha sel
- #AUTOR:		
- #FECHA:		23/05/11	
-***********************************/
-     elsif(par_transaccion='RH_GETCUMPLEA_SEL')then
+        v_consulta := v_consulta || v_parametros.filtro;
 
-          --consulta:=';
-          BEGIN
+        return v_consulta;
 
-               v_consulta:='SELECT 
+
+      END;
+
+    /*******************************
+     #TRANSACCION:  RH_GETCUMPLEA_SEL
+     #DESCRIPCION:	Cumpleaneros a fecha sel
+     #AUTOR:
+     #FECHA:		23/05/11
+    ***********************************/
+    elsif(par_transaccion='RH_GETCUMPLEA_SEL')then
+
+      --consulta:=';
+      BEGIN
+
+        v_consulta:='SELECT
                				FUNCIO.id_funcionario,
                             FUNCIO.desc_funcionario1::varchar,
                             CAR.nombre,                            
@@ -251,31 +251,31 @@ BEGIN
                                 (uofun.fecha_finalizacion >= now()::date or uofun.fecha_finalizacion is null)
                             INNER JOIN orga.tcargo car on car.id_cargo = uofun.id_cargo                            
                             WHERE ';
-               
-               
-               
-               v_consulta := v_consulta || v_parametros.filtro;
-                            
-               
-               v_consulta:=v_consulta||' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' OFFSET ' || v_parametros.puntero;
-
-               return v_consulta;
 
 
-         END;
 
-/*******************************
- #TRANSACCION:  RH_GETCUMPLEA_CONT
- #DESCRIPCION:	Conteo de empleados que cumplen anos a una fecha
- #AUTOR:		
- #FECHA:		23/05/11	
-***********************************/
-     elsif(par_transaccion='RH_GETCUMPLEA_CONT')then
+        v_consulta := v_consulta || v_parametros.filtro;
 
-          --consulta:=';
-          BEGIN
 
-               v_consulta:='SELECT 
+        v_consulta:=v_consulta||' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' OFFSET ' || v_parametros.puntero;
+
+        return v_consulta;
+
+
+      END;
+
+    /*******************************
+     #TRANSACCION:  RH_GETCUMPLEA_CONT
+     #DESCRIPCION:	Conteo de empleados que cumplen anos a una fecha
+     #AUTOR:
+     #FECHA:		23/05/11
+    ***********************************/
+    elsif(par_transaccion='RH_GETCUMPLEA_CONT')then
+
+      --consulta:=';
+      BEGIN
+
+        v_consulta:='SELECT
                				count(FUNCIO.id_funcionario)
                                                         
                             FROM orga.vfuncionario FUNCIO
@@ -288,33 +288,33 @@ BEGIN
                             INNER JOIN orga.tcargo car on car.id_cargo = uofun.id_cargo      
                             
                             WHERE ';
-               
-               
-               
-               v_consulta := v_consulta || v_parametros.filtro;
-               
-               return v_consulta;
 
 
-         END;
-    
+
+        v_consulta := v_consulta || v_parametros.filtro;
+
+        return v_consulta;
+
+
+      END;
+
     /*******************************
      #TRANSACCION:  RH_FUNCIOCAR_SEL
      #DESCRIPCION:	Listado de funcionarios con cargos historicos
      #AUTOR:		KPLIAN (RAC)
      #FECHA:		29/10/11	
     ***********************************/
-     elseif(par_transaccion='RH_FUNCIOCAR_SEL')then
+    elseif(par_transaccion='RH_FUNCIOCAR_SEL')then
 
-          --consulta:=';
-          BEGIN
-          
-           v_filadd = '';
-            IF (pxp.f_existe_parametro(par_tabla,'estado_reg_asi')) THEN
-               v_filadd = ' (FUNCAR.estado_reg_asi = '''||v_parametros.estado_reg_asi||''') and ';
-            END IF;
+      --consulta:=';
+      BEGIN
 
-               v_consulta:='SELECT 
+        v_filadd = '';
+        IF (pxp.f_existe_parametro(par_tabla,'estado_reg_asi')) THEN
+          v_filadd = ' (FUNCAR.estado_reg_asi = '''||v_parametros.estado_reg_asi||''') and ';
+        END IF;
+
+        v_consulta:='SELECT
                             FUNCAR.id_uo_funcionario,
                             FUNCAR.id_funcionario,
                             FUNCAR.desc_funcionario1,
@@ -339,62 +339,62 @@ BEGIN
                             
                             FROM orga.vfuncionario_cargo_lugar FUNCAR 
                             WHERE '||v_filadd;
-               
-               
-               v_consulta:=v_consulta||v_parametros.filtro;
-               v_consulta:=v_consulta||' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' OFFSET ' || v_parametros.puntero;
-
-              
-                     
-               return v_consulta;
 
 
-         END;
+        v_consulta:=v_consulta||v_parametros.filtro;
+        v_consulta:=v_consulta||' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' OFFSET ' || v_parametros.puntero;
 
-/*******************************
- #TRANSACCION:  RH_FUNCIOCAR_CONT
- #DESCRIPCION:	Conteo de funcionarios con cargos historicos
- #AUTOR:		KPLIAN (rac)
- #FECHA:		23/05/11	
-***********************************/
-     elsif(par_transaccion='RH_FUNCIOCAR_CONT')then
 
-          --consulta:=';
-          BEGIN
-          
-            v_filadd = '';
-            IF (pxp.f_existe_parametro(par_tabla,'estado_reg_asi')) THEN
-               v_filadd = ' (FUNCAR.estado_reg_asi = '''||v_parametros.estado_reg_asi||''') and ';
-            END IF;
 
-               v_consulta:='SELECT
+        return v_consulta;
+
+
+      END;
+
+    /*******************************
+     #TRANSACCION:  RH_FUNCIOCAR_CONT
+     #DESCRIPCION:	Conteo de funcionarios con cargos historicos
+     #AUTOR:		KPLIAN (rac)
+     #FECHA:		23/05/11
+    ***********************************/
+    elsif(par_transaccion='RH_FUNCIOCAR_CONT')then
+
+      --consulta:=';
+      BEGIN
+
+        v_filadd = '';
+        IF (pxp.f_existe_parametro(par_tabla,'estado_reg_asi')) THEN
+          v_filadd = ' (FUNCAR.estado_reg_asi = '''||v_parametros.estado_reg_asi||''') and ';
+        END IF;
+
+        v_consulta:='SELECT
                                   count(id_uo_funcionario)
                             FROM orga.vfuncionario_cargo_lugar FUNCAR 
                             WHERE '||v_filadd;
-               v_consulta:=v_consulta||v_parametros.filtro;
-               return v_consulta;
-         END;   
+        v_consulta:=v_consulta||v_parametros.filtro;
+        return v_consulta;
+      END;
 
-      else 
-         raise exception 'No existe la opcion';
+    else
+      raise exception 'No existe la opcion';
 
-     end if;
-     
-     
-     
-
-EXCEPTION
-
-      WHEN OTHERS THEN
-    	v_resp='';
-		v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
-    	v_resp = pxp.f_agrega_clave(v_resp,'codigo_error',SQLSTATE);
-  		v_resp = pxp.f_agrega_clave(v_resp,'procedimientos',v_nombre_funcion);
-		raise exception '%',v_resp;
+    end if;
 
 
-END;
-$body$
+
+
+    EXCEPTION
+
+    WHEN OTHERS THEN
+      v_resp='';
+      v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
+      v_resp = pxp.f_agrega_clave(v_resp,'codigo_error',SQLSTATE);
+      v_resp = pxp.f_agrega_clave(v_resp,'procedimientos',v_nombre_funcion);
+      raise exception '%',v_resp;
+
+
+  END;
+  $body$
 LANGUAGE 'plpgsql'
 VOLATILE
 CALLED ON NULL INPUT
