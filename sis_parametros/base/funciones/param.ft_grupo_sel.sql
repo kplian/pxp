f8c463af453fd663eca4cdf370ce2548ae425a74
Jaime Rivera <rensi4rn@gmail.com>
2017-03-06 09:55:39 -0400

    interfaz detalles usuarios grupo

--- sis_parametros/base/funciones/param.ft_grupo_sel.sql
+++ sis_parametros/base/funciones/param.ft_grupo_sel.sql
@@ -1,20 +1,24 @@
-CREATE OR REPLACE FUNCTION "param"."ft_grupo_sel"(	
-				p_administrador integer, p_id_usuario integer, p_tabla character varying, p_transaccion character varying)
-RETURNS character varying AS
-$BODY$
+CREATE OR REPLACE FUNCTION param.ft_grupo_sel (
+  p_administrador integer,
+  p_id_usuario integer,
+  p_tabla varchar,
+  p_transaccion varchar
+)
+RETURNS varchar AS
+$body$
 /**************************************************************************
  SISTEMA:		Parametros Generales
  FUNCION: 		param.ft_grupo_sel
  DESCRIPCION:   Funcion que devuelve conjuntos de registros de las consultas relacionadas con la tabla 'param.tgrupo'
  AUTOR: 		 (admin)
  FECHA:	        22-04-2013 14:20:57
- COMENTARIOS:	
+ COMENTARIOS:
 ***************************************************************************
  HISTORIAL DE MODIFICACIONES:
 
- DESCRIPCION:	
- AUTOR:			
- FECHA:		
+ DESCRIPCION:
+ AUTOR:
+ FECHA:
 ***************************************************************************/
 
 DECLARE
@@ -23,21 +27,21 @@ DECLARE
 	v_parametros  		record;
 	v_nombre_funcion   	text;
 	v_resp				varchar;
-			    
+
 BEGIN
 
 	v_nombre_funcion = 'param.ft_grupo_sel';
     v_parametros = pxp.f_get_record(p_tabla);
 
-	/*********************************    
+	/*********************************
  	#TRANSACCION:  'PM_GRU_SEL'
  	#DESCRIPCION:	Consulta de datos
- 	#AUTOR:		admin	
+ 	#AUTOR:		admin
  	#FECHA:		22-04-2013 14:20:57
 	***********************************/
 
 	if(p_transaccion='PM_GRU_SEL')then
-     				
+
     	begin
     		--Sentencia de la consulta
 			v_consulta:='select
@@ -50,25 +54,37 @@ BEGIN
 						gru.fecha_mod,
 						gru.id_usuario_mod,
 						usu1.cuenta as usr_reg,
-						usu2.cuenta as usr_mod	
+						usu2.cuenta as usr_mod,
+                        pxp.text_concat(uep.id_usuario::text) as id_usuario
 						from param.tgrupo gru
 						inner join segu.tusuario usu1 on usu1.id_usuario = gru.id_usuario_reg
 						left join segu.tusuario usu2 on usu2.id_usuario = gru.id_usuario_mod
+                        inner join segu.tusuario_grupo_ep uep on uep.id_grupo = gru.id_grupo
 				        where  ';
-			
+
 			--Definicion de la respuesta
 			v_consulta:=v_consulta||v_parametros.filtro;
+            v_consulta:=v_consulta||' GROUP BY gru.id_grupo,
+						gru.estado_reg,
+						gru.nombre,
+						gru.obs,
+						gru.fecha_reg,
+						gru.id_usuario_reg,
+						gru.fecha_mod,
+						gru.id_usuario_mod,
+						usu1.cuenta,
+						usu2.cuenta';
 			v_consulta:=v_consulta||' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' offset ' || v_parametros.puntero;
 
 			--Devuelve la respuesta
 			return v_consulta;
-						
+
 		end;
 
-	/*********************************    
+	/*********************************
  	#TRANSACCION:  'PM_GRU_CONT'
  	#DESCRIPCION:	Conteo de registros
- 	#AUTOR:		admin	
+ 	#AUTOR:		admin
  	#FECHA:		22-04-2013 14:20:57
 	***********************************/
 
@@ -81,23 +97,23 @@ BEGIN
 					    inner join segu.tusuario usu1 on usu1.id_usuario = gru.id_usuario_reg
 						left join segu.tusuario usu2 on usu2.id_usuario = gru.id_usuario_mod
 					    where ';
-			
-			--Definicion de la respuesta		    
+
+			--Definicion de la respuesta
 			v_consulta:=v_consulta||v_parametros.filtro;
 
 			--Devuelve la respuesta
 			return v_consulta;
 
 		end;
-					
+
 	else
-					     
+
 		raise exception 'Transaccion inexistente';
-					         
+
 	end if;
-					
+
 EXCEPTION
-					
+
 	WHEN OTHERS THEN
 			v_resp='';
 			v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
@@ -105,7 +121,9 @@ EXCEPTION
 			v_resp = pxp.f_agrega_clave(v_resp,'procedimientos',v_nombre_funcion);
 			raise exception '%',v_resp;
 END;
-$BODY$
-LANGUAGE 'plpgsql' VOLATILE
+$body$
+LANGUAGE 'plpgsql'
+VOLATILE
+CALLED ON NULL INPUT
+SECURITY INVOKER
 COST 100;
-ALTER FUNCTION "param"."ft_grupo_sel"(integer, integer, character varying, character varying) OWNER TO postgres;
