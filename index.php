88bffa956bd754a96ade57cc3f54de19730b7cb0
Jaime Rivera <jriverarojas@gmail.com>
2017-06-23 12:39:19 -0400

    Update index.php
    Adicionando  licencia

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
-//include_once('lib/lib_general/Errores.php');
+
 /*
-register_shutdown_function('fatalErrorShutdownHandler');
-set_exception_handler('exception_handler');
-set_error_handler('error_handler');
+Copyright (C) 2017  Kplian Ltda.
+
+This file is part of PXP.
+
+PXP is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PXP is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PXP.  If not, see <http://www.gnu.org/licenses/>.
+    
 */
 //Jaime:Se incluye datos generales de conexion y otros de configuracion
 header('Access-Control-Allow-Origin: *');
