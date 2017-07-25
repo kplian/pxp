727480a8fd30d33b2d06a22190f817afbd3fda75
Rodrigo Chumacero <rodrychm@gmail.com>
2017-07-25 07:35:26 -0400

    correccion phx.cp

--- sis_seguridad/vista/_adm/resources/Phx.CP.js
+++ sis_seguridad/vista/_adm/resources/Phx.CP.js
@@ -1736,33 +1736,7 @@ Phx.CP=function(){
                     //parseMargins:'50 50 50 50',
                     minimizable: false,
                     maximizable: true,
-                    proxyDrag:true,
-                    bbar: {
-                        items: [
-                            {
-                                // xtype: 'button', // default for Toolbars, same as 'tbbutton'
-                                text: 'Button'
-                            },
-                            {
-                                xtype: 'splitbutton', // same as 'tbsplitbutton'
-                                text: 'Split Button'
-                            },
-                            // begin using the right-justified button container
-                            '->', // same as {xtype: 'tbfill'}, // Ext.Toolbar.Fill
-                            {
-                                xtype: 'textfield',
-                                name: 'field1',
-                                emptyText: 'enter search term'
-                            },
-                            // add a vertical separator bar between toolbar items
-                            '-', // same as {xtype: 'tbseparator'} to create Ext.Toolbar.Separator
-                            'text 1', // same as {xtype: 'tbtext', text: 'text1'} to create Ext.Toolbar.TextItem
-                            {xtype: 'tbspacer'},// same as ' ' to create Ext.Toolbar.Spacer
-                            'text 2',
-                            {xtype: 'tbspacer', width: 50}, // add a 50px space
-                            'text 3'
-                        ]
-                    }
+                    proxyDrag:true
                 };
 
                 Ext.apply(Ventana,config);
