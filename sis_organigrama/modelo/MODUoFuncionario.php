44ffe3bed83d802de7549a95c134a41d32cc3a33
Rodrigo Chumacero <rodrychm@gmail.com>
2019-01-09 17:22:58 -0400

    #6 recupera funcion de listado desde repositorio de boa

--- sis_organigrama/modelo/MODUoFuncionario.php
+++ sis_organigrama/modelo/MODUoFuncionario.php
@@ -4,6 +4,14 @@
  Proposito: Clase de Modelo, que contiene la definicion y llamada a funciones especificas relacionadas a la tabla tuo_funcionario del esquema RHUM
  Autor:		Kplian
  Fecha:		04/06/2011
+ * 
+ * 
+ * ***************************************************************************************************   
+    HISTORIAL DE MODIFICACIONES:
+       
+ ISSUE            FECHA:              AUTOR                 DESCRIPCION
+   
+ #6           09/01/2019      RAC KPLIAN      añade listarAsignacionFuncionario
  */
 class MODUoFuncionario extends MODbase{
 	
@@ -117,6 +125,45 @@ class MODUoFuncionario extends MODbase{
 		$this->ejecutarConsulta();
 		return $this->respuesta;
 	}
+	
+	//#6
+	function listarAsignacionFuncionario(){
+        //Definicion de variables para ejecucion del procedimiento
+        $this->procedimiento='orga.ft_uo_funcionario_sel';// nombre procedimiento almacenado
+        $this->transaccion='RH_ASIG_FUNC_SEL';//nombre de la transaccion
+        $this->tipo_procedimiento='SEL';//tipo de transaccion
+        $this->setParametro('id_uo','id_uo','integer');
+        //Definicion de la lista del resultado del query
+        $this->captura('id_uo_funcionario','integer');
+        $this->captura('id_uo','integer');
+        $this->captura('id_funcionario','integer');
+        $this->captura('ci','varchar');
+        $this->captura('codigo','varchar');
+        $this->captura('desc_funcionario1','text');
+        $this->captura('desc_funcionario2','text');
+        $this->captura('num_doc','integer');
+        $this->captura('fecha_asignacion','date');
+        $this->captura('fecha_finalizacion','date');
+        $this->captura('estado_reg','varchar');
+        $this->captura('fecha_mod','timestamp');
+        $this->captura('fecha_reg','timestamp');
+        $this->captura('id_usuario_mod','integer');
+        $this->captura('id_usuario_reg','integer');
+        $this->captura('USUREG','text');
+        $this->captura('USUMOD','text');
+        $this->captura('id_cargo','integer');
+        $this->captura('desc_cargo','text');
+        $this->captura('observaciones_finalizacion','varchar');
+        $this->captura('nro_documento_asignacion','varchar');
+        $this->captura('fecha_documento_asignacion','date');
+        $this->captura('tipo','varchar');
+        $this->captura('haber_basico','numeric');
+        $this->captura('tipo_contrato','varchar');
+        //Ejecuta la funcion
+        $this->armarConsulta();
+        $this->ejecutarConsulta();
+        return $this->respuesta;
+    }
 	
 }
 ?>
