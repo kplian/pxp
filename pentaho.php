e0e81441b35442ca606073de45752a99d8610c60
Rodrigo Chumacero <rodrychm@gmail.com>
2019-04-02 09:41:40 -0400

    close #14 endetr agregado de menu pentaho

---
+++ pentaho.php
@@ -0,0 +1,15 @@
+<!-- #14 endetr Juan Agregado de menu pentaho -->
+<!doctype html>
+<html>
+  <head>
+    <title>Cubos</title>
+    <link rel="stylesheet" href="../assets/ol3/css/ol.css" type="text/css" />
+    <link rel="stylesheet" href="../assets/css/samples.css" type="text/css" />
+  </head>
+  <body>
+  <body>
+      <iframe src="http://172.18.79.98:8080/pentaho/content/saiku-ui/index.html?biplugin5=true&dimension_prefetch=false#query/open/%3Ahome%3Ajfigueroa%3AInversiones_ETR_IVA.saiku" width="100%" height="800px" align="center"> 
+
+      </iframe>
+  </body>
+</html>
