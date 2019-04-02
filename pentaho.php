a309ca82b7ee72a98158f4b84c30d4676ba38783
Rodrigo Chumacero <rodrychm@gmail.com>
2019-04-02 12:17:47 -0400

    #14 endetr correccion de url pentaho

--- pentaho.php
+++ pentaho.php
@@ -1,3 +1,8 @@
+
+<?php 
+require_once(dirname(__FILE__).'../../pxp/lib/DatosGenerales.php');
+?>
+
 <!-- #14 endetr Juan Agregado de menu pentaho -->
 <!doctype html>
 <html>
@@ -8,8 +13,9 @@
   </head>
   <body>
   <body>
-      <iframe src="http://172.18.79.98:8080/pentaho/content/saiku-ui/index.html?biplugin5=true&dimension_prefetch=false#query/open/%3Ahome%3Ajfigueroa%3AInversiones_ETR_IVA.saiku" width="100%" height="800px" align="center"> 
+      <iframe src="http://<?php echo $_SESSION['_HOST_SERVIDOR_PENTAHO'];?>/pentaho/Login" width="100%" height="800px" align="center"> 
 
       </iframe>
   </body>
 </html>
+
