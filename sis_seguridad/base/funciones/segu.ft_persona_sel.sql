accca3a6e79e14432cbf7b80a75f983ed1bcd650
Rodrigo Chumacero <rodrychm@gmail.com>
2019-12-02 18:36:56 -0400

    #88 Correccion consulta

--- sis_seguridad/base/funciones/segu.ft_persona_sel.sql
+++ sis_seguridad/base/funciones/segu.ft_persona_sel.sql
@@ -1,27 +1,19 @@
-CREATE OR REPLACE FUNCTION segu.ft_persona_sel (
-  par_administrador integer,
-  par_id_usuario integer,
-  par_tabla varchar,
-  par_transaccion varchar
-)
-RETURNS varchar AS
-$body$
 /**************************************************************************
  FUNCION: 		segu.ft_persona_sel
  DESCRIPCION:   consultas de persona
- AUTOR: 		KPLIAN(jrr)	
- FECHA:	
- COMENTARIOS:	
+ AUTOR: 		KPLIAN(jrr)
+ FECHA:
+ COMENTARIOS:
 ***************************************************************************
  HISTORIA DE MODIFICACIONES:
 
  DESCRIPCION:	actualizacion a nueva version xph
- AUTOR:		KPLIAN(jrr)		
+ AUTOR:		KPLIAN(jrr)
  FECHA:		08/01/11
- 
 
-       
- ISSUE            FECHA:         EMPRESA     AUTOR                 DESCRIPCION  
+
+
+ ISSUE            FECHA:         EMPRESA     AUTOR                 DESCRIPCION
   #40            31-07-2019      ETR          mzm                SEG_PERSONMIN_SEL:adicion de campos matricula, historia_clinica, fecha_nacimiento
   #55			 02.09.2019		 ETR		  MZM				 Adicion de campo abreviatura_titulo
 ***************************************************************************/
@@ -55,15 +47,15 @@ BEGIN
  /*******************************
  #TRANSACCION:  SEG_PERSON_SEL
  #DESCRIPCION:	Selecciona Personas
- #AUTOR:		KPLIAN(jrr)	
- #FECHA:		08/01/11	
+ #AUTOR:		KPLIAN(jrr)
+ #FECHA:		08/01/11
 ***********************************/
      if(par_transaccion='SEG_PERSON_SEL')then
 
           --  Se arma la consulta de personas
           BEGIN
 
-               v_consulta:='SELECT 
+               v_consulta:='SELECT
                               p.id_persona,
                               p.ap_materno,
                               p.ap_paterno,
@@ -82,9 +74,9 @@ BEGIN
                               per.direccion,
                               per.tipo_documento,
                               per.expedicion
-                              
-                          FROM segu.vpersona p 
-                          inner join segu.tpersona per on per.id_persona = p.id_persona 
+
+                          FROM segu.vpersona p
+                          inner join segu.tpersona per on per.id_persona = p.id_persona
                           WHERE ';
                v_consulta:=v_consulta||v_parametros.filtro;
                v_consulta:=v_consulta || ' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' OFFSET ' || v_parametros.puntero;
@@ -93,42 +85,42 @@ BEGIN
 
 
          END;
-         
-         
+
+
 
 
 /*******************************
  #TRANSACCION:  SEG_PERSON_CONT
  #DESCRIPCION:	Cuenta Personas
- #AUTOR:		KPLIAN(jrr)		
- #FECHA:		08/01/11	
+ #AUTOR:		KPLIAN(jrr)
+ #FECHA:		08/01/11
 ***********************************/
 
      elsif(par_transaccion='SEG_PERSON_CONT')then
 
           --se arma la sonsulta que cuenta personas
           BEGIN
-               
+
                v_consulta:='select count(p.id_persona)
-               				FROM segu.vpersona p 
-                          	inner join segu.tpersona per on per.id_persona = p.id_persona 
+               				FROM segu.vpersona p
+                          	inner join segu.tpersona per on per.id_persona = p.id_persona
                           	WHERE ';
                v_consulta:=v_consulta||v_parametros.filtro;
                return v_consulta;
          END;
-         
-         
+
+
  /*******************************
  #TRANSACCION:  SEG_PERSONMIN_SEL
  #DESCRIPCION:	Selecciona Personas + fotografia
- #AUTOR:		KPLIAN(rac)	
- #FECHA:		08/01/11	
+ #AUTOR:		KPLIAN(rac)
+ #FECHA:		08/01/11
 ***********************************/
      elsif(par_transaccion='SEG_PERSONMIN_SEL')then
 
           --  Se arma la consulta de personas
           BEGIN
-          
+
                v_consulta:='SELECT p.id_persona,
                              p.apellido_materno AS ap_materno,
                              p.apellido_paterno AS ap_paterno,
@@ -147,7 +139,8 @@ BEGIN
                              p.num_documento,
                              p.telefono1,
                              p.telefono2,
-                             p.celular2,                             
+                             p.celular2,
+                             p.direccion,  --#88
                              p.extension,
                              p.tipo_documento,
                              p.expedicion,
@@ -162,59 +155,59 @@ BEGIN
                              , p.profesion
                              --#59 09.09.2019
                           FROM segu.tpersona p WHERE ';
-                          
-                         
+
+
                v_consulta:=v_consulta||v_parametros.filtro;
                v_consulta:=v_consulta || ' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' OFFSET ' || v_parametros.puntero;
                --raise exception '%',v_consulta;
                return v_consulta;
 
 
-         END;    
+         END;
 /*******************************
  #TRANSACCION:  SEG_OPERFOT_SEL
  #DESCRIPCION:	Selecciona Personas + fotografia
- #AUTOR:		KPLIAN(rac)	
- #FECHA:		23/04/13	
+ #AUTOR:		KPLIAN(rac)
+ #FECHA:		23/04/13
 ***********************************/
      elsif(par_transaccion='SEG_OPERFOT_SEL')then
 
           --  Se arma la consulta de personas
           BEGIN
-          
+
                v_consulta:='SELECT p.id_persona,
                              p.extension,
                              p.foto
-                          FROM segu.tpersona p 
-                          inner join segu.tusuario u on u.id_persona = p.id_persona    
+                          FROM segu.tpersona p
+                          inner join segu.tusuario u on u.id_persona = p.id_persona
                           WHERE u.id_usuario='||v_parametros.id_usuario;
-             
+
                 raise notice '%',v_consulta;
-               
+
                 return v_consulta;
 
 
-         END;    
+         END;
 
 /*******************************
  #TRANSACCION:  SEG_PERSONMIN_CONT
  #DESCRIPCION:	Cuenta Personas con foto
- #AUTOR:		KPLIAN(jrr)		
- #FECHA:		08/01/11	
+ #AUTOR:		KPLIAN(jrr)
+ #FECHA:		08/01/11
 ***********************************/
 
      elsif(par_transaccion='SEG_PERSONMIN_CONT')then
-                            
+
 
           --se arma la sonsulta que cuenta personas
           BEGIN
-               
+
                v_consulta:='select count(p.id_persona)
                from segu.tpersona p where ';
                v_consulta:=v_consulta||v_parametros.filtro;
                return v_consulta;
          END;
-         
+
      else
          raise exception 'No existe la opcion';
 
@@ -231,9 +224,3 @@ EXCEPTION
 
 
 END;
-$body$
-LANGUAGE 'plpgsql'
-VOLATILE
-CALLED ON NULL INPUT
-SECURITY INVOKER
-COST 100;
