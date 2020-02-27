9e21b243f561de25f33b709286d303d1d237e66f
mzambrana <mzambrana@endetransmision.bo>
2020-02-27 07:46:03 -0400

    #116 adicion de filtro fecha en CC para obtencion de CC cuya gestion contemple la fecha seleccionada, para vista venta peaje ETR de sis_ventas_facturacion

--- sis_parametros/control/ACTCentroCosto.php
+++ sis_parametros/control/ACTCentroCosto.php
@@ -11,6 +11,7 @@
   #1			     	19-02-2013			RAC							creacion
   #37   	ENDETR      12/02/2019         	RAC KPLIAN                  que no filtre centros de costo pr aprobados, adicion de parametro
  *#27		ETR 		27/06/2019			RCM 						Corrección filtro por fecha
+ *#116		ETR			26/02/2020			MZM							Adicion de filtro fecha para CC filtrado por usuario, de modo que liste solo los CC cuya fecha enviada pertenezca a la gestion  
  * *************************************************************************/
 
 class ACTCentroCosto extends ACTbase{
@@ -182,7 +183,11 @@ class ACTCentroCosto extends ACTbase{
 		if($this->objParam->getParametro('aprobado') == 'si'){
 			$this->objParam->addFiltro("cec.estado = ''aprobado''");
 		}
-
+		//#116
+		if($this->objParam->getParametro('fecha')!=''){
+	    	$this->objParam->addFiltro("cec.id_gestion in (select id_gestion from param.tgestion where gestion = extract(''year'' from ''" . $this->objParam->getParametro('fecha') . "''::date))"); //#27 Faltaba un paréntesis de cerrado y el cast a date
+		}
+		
         $this->objParam->addFiltro("cec.estado_reg = ''activo''");
         $this->objParam->defecto('dir_ordenacion','asc');
         if($this->objParam->getParametro('id_gestion')!=''){
