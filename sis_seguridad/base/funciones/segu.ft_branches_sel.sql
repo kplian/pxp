b7653efec1b8ec936ebaffd6ab4adac8ab6cd827
favio figueroa <fabiofigueroap@gmail.com>
2020-05-14 18:56:50 +0000

    solucionar problemas de restauracion

--- sis_seguridad/base/funciones/segu.ft_branches_sel.sql
+++ sis_seguridad/base/funciones/segu.ft_branches_sel.sql
@@ -9,14 +9,14 @@ $body$
 /**************************************************************************
  SISTEMA:		Sistema de Seguridad
  FUNCION: 		segu.ft_branches_sel
- DESCRIPCION:   Funcion que devuelve conjuntos de registros de las consultas relacionadas con la tabla ''segu.tbranches''
+ DESCRIPCION:   Funcion que devuelve conjuntos de registros de las consultas relacionadas con la tabla 'segu.tbranches'
  AUTOR: 		 (miguel.mamani)
  FECHA:	        09-01-2020 21:26:12
  COMENTARIOS:
 ***************************************************************************
  HISTORIAL DE MODIFICACIONES:
 #ISSUE				FECHA				AUTOR				DESCRIPCION
- #0				09-01-2020 21:26:12								Funcion que devuelve conjuntos de registros de las consultas relacionadas con la tabla ''segu.tbranches''
+ #0				09-01-2020 21:26:12								Funcion que devuelve conjuntos de registros de las consultas relacionadas con la tabla 'segu.tbranches'
  #104			27-02-2020 				MMV ETR     			Import github commit data, problems, branch and repository
  ***************************************************************************/
 
@@ -29,52 +29,52 @@ DECLARE
 
 BEGIN
 
-	v_nombre_funcion = ''segu.ft_branches_sel'';
+	v_nombre_funcion = 'segu.ft_branches_sel';
     v_parametros = pxp.f_get_record(p_tabla);
 
 	/*********************************
- 	#TRANSACCION:  ''SG_BAS_SEL''
+ 	#TRANSACCION:  'SG_BAS_SEL'
  	#DESCRIPCION:	Consulta de datos
  	#AUTOR:		miguel.mamani	 #104
  	#FECHA:		09-01-2020 21:26:12
 	***********************************/
 
-	if(p_transaccion=''SG_BAS_SEL'')then
+	if(p_transaccion='SG_BAS_SEL')then
 
     	begin
     		--Sentencia de la consulta
-			v_consulta:=''select	id_branches,
+			v_consulta:='select	id_branches,
                                 name,
                                 sha,
                                 url,
                                 protected,
                                 id_subsistema
                                 from segu.tbranches
-                                where  '';
+                                where  ';
 
 			--Definicion de la respuesta
 			v_consulta:=v_consulta||v_parametros.filtro;
-			v_consulta:=v_consulta||'' order by '' ||v_parametros.ordenacion|| '' '' || v_parametros.dir_ordenacion || '' limit '' || v_parametros.cantidad || '' offset '' || v_parametros.puntero;
-			raise notice ''%'',v_consulta;
+			v_consulta:=v_consulta||' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' offset ' || v_parametros.puntero;
+			raise notice '%',v_consulta;
 			--Devuelve la respuesta
 			return v_consulta;
 
 		end;
 
 	/*********************************
- 	#TRANSACCION:  ''SG_BAS_CONT''  #104
+ 	#TRANSACCION:  'SG_BAS_CONT'  #104
  	#DESCRIPCION:	Conteo de registros
  	#AUTOR:		miguel.mamani
  	#FECHA:		09-01-2020 21:26:12
 	***********************************/
 
-	elsif(p_transaccion=''SG_BAS_CONT'')then
+	elsif(p_transaccion='SG_BAS_CONT')then
 
 		begin
 			--Sentencia de la consulta de conteo de registros
-			v_consulta:=''select count(id_branches)
+			v_consulta:='select count(id_branches)
 					    from segu.tbranches
-					    where '';
+					    where ';
 
 			--Definicion de la respuesta
 			v_consulta:=v_consulta||v_parametros.filtro;
@@ -86,25 +86,22 @@ BEGIN
 
 	else
 
-		raise exception ''Transaccion inexistente'';
+		raise exception 'Transaccion inexistente';
 
 	end if;
 
 EXCEPTION
 
 	WHEN OTHERS THEN
-			v_resp='''';
-			v_resp = pxp.f_agrega_clave(v_resp,''mensaje'',SQLERRM);
-			v_resp = pxp.f_agrega_clave(v_resp,''codigo_error'',SQLSTATE);
-			v_resp = pxp.f_agrega_clave(v_resp,''procedimientos'',v_nombre_funcion);
-			raise exception ''%'',v_resp;
+			v_resp='';
+			v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
+			v_resp = pxp.f_agrega_clave(v_resp,'codigo_error',SQLSTATE);
+			v_resp = pxp.f_agrega_clave(v_resp,'procedimientos',v_nombre_funcion);
+			raise exception '%',v_resp;
 END;
 $body$
-LANGUAGE ''plpgsql''
+LANGUAGE 'plpgsql'
 VOLATILE
 CALLED ON NULL INPUT
 SECURITY INVOKER
 COST 100;
-
-ALTER FUNCTION segu.ft_branches_sel (p_administrador integer, p_id_usuario integer, p_tabla varchar, p_transaccion varchar)
-  OWNER TO postgres;
