af1b6bba47a0e93613ec5128be072b3d5d1f2b0d
Administrator <Administrator@endetransmision.bo>
2020-09-27 19:48:28 -0400

    MSA-34 Se agrega el archivo para actualizacion de datos en BD

---
+++ sis_organigrama/base/dataupdate000001.sql
@@ -0,0 +1,3 @@
+/********************************************I-DAUP-AUTOR-SCHEMA-0-31/02/2019********************************************/
+--SHEMA : Esquema (CONTA) contabilidad         AUTHOR:Siglas del autor de los scripts' dataupdate000001.txt
+/********************************************F-DAUP-AUTOR-SCHEMA-0-31/02/2019********************************************/
