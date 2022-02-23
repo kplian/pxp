20859a750c3947dd2276bf423e2023490c4a5830
zamerc <mercedes@kplian.com>
2022-02-23 01:14:52 -0400

    #0 borrado de funcion monitor

--- base/funciones/pxp.monitor_phx.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION pxp.monitor_phx (
-  integer
-)
-RETURNS integer
-AS '/usr/local/lib/phx.so', 'monitor_phx'
-    LANGUAGE c STRICT;
---
--- Definition for function trigger_usuario (OID = 304916) : 
---
