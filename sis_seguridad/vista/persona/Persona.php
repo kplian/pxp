3d7b5ceddea3da002df0371de80f6e6f3fc40c70
Rodrigo Chumacero <rodrigochm>
2025-08-07 08:04:23 -0400

    alloe blanc in persona

--- sis_seguridad/vista/persona/Persona.php
+++ sis_seguridad/vista/persona/Persona.php
@@ -124,7 +124,7 @@ Phx.vista.persona=Ext.extend(Phx.gridInterfaz,{
                 fieldLabel: "Sobre Nombre",
                 gwidth: 130,
                 name: 'sobrenombre',
-                allowBlank:false,
+                allowBlank:true,
                 maxLength:50,
 
                 anchor:'100%'
@@ -141,7 +141,7 @@ Phx.vista.persona=Ext.extend(Phx.gridInterfaz,{
                 fieldLabel: "Cualidad 1",
                 gwidth: 130,
                 name: 'cualidad_1',
-                allowBlank:false,
+                allowBlank:true,
                 maxLength:50,
 
                 anchor:'100%'
@@ -158,7 +158,7 @@ Phx.vista.persona=Ext.extend(Phx.gridInterfaz,{
                 fieldLabel: "Cualidad 2",
                 gwidth: 130,
                 name: 'cualidad_2',
-                allowBlank:false,
+                allowBlank:true,
                 maxLength:50,
 
                 anchor:'100%'
